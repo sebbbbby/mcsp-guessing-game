12bca8acae9e13d80185ddfd8f01458c99ae7f9d
s p <sp@ss-MacBook-Air.local>
2023-04-04 12:40:32 -0400

    final

--- app.js
+++ app.js
@@ -1,24 +1,12 @@
-let name = prompt('Please Enter Your Name')
-let name1 = name.toUpperCase()
+let players = {}
+
 function guessingGame() {
-    let input = prompt('Guess a number 1-50 !')
+    let name1 = prompt('Please Enter Your Name')
+    let input = prompt('Guess a number 1-50 !') //could be number=promptInt('insert prompt') this will return an integer as the answer instead of two variables doing this
     let number = Number(input)
     let ans = 22 //Math.floor(Math.random() * 50 + 1);
     let guessCount = 1
     let prevGuess = [number] //number is nested in here since the first input isnt calculated into prevGuess without this additions of [numbers]
-
-    // //problem is that if you click cancel, it will be considered a low number
-    // while (number !== ans) {
-    //   if (!Number.isInteger(number)) {
-    //     number = Number(prompt("Give me a whole number"));
-    //     //   prevGuess.push(number);
-    //     //   guessCount += 1;
-    //     //these codes were redudant and the codes below adjust for it
-    //   } else if (number > ans) {
-    //     number = Number(prompt(" You went too high! Give me a lower number"));
-    //   } else if (number < ans) {
-    //     number = Number(prompt(" You went too low! Give me a higher number"));
-    //   }
     //the code below helped with the fact i could not cancel out
     //the return allows for moving forward with the function, MUST be in a function
     while (number !== ans) {
@@ -49,6 +37,7 @@ function guessingGame() {
             `Correct! It took you ${guessCount} tries! Your previous guesses were ${prevGuess}`
         )
     }
+
     //keeping the function allowed for continue to play until you click 'cancel'
     function playAgain() {
         if (confirm('Do you want to play again? Click OK for Yes') === true) {
@@ -58,9 +47,30 @@ function guessingGame() {
             return
         }
     }
+
+    if (players[name1] !== undefined) {
+        let numGuesses = prevGuess.length
+        let previousGuesses = players[name1].length
+        console.log(previousGuesses)
+        if (numGuesses > previousGuesses) {
+            alert(
+                `That is correct ${name1} ! you did better in your last game by ${
+                    numGuesses - previousGuesses
+                }`
+            )
+        } else if (numGuesses < previousGuesses) {
+            alert(
+                `That is Correct ${name1}! and you beat your previous attempt by ${
+                    previousGuesses - numGuesses
+                } fewer guesses`
+            )
+        }
+    }
+    players[name1] = prevGuess
+
     playAgain()
 }
 
 guessingGame()
 
-debugger
+//debugger //debugger lets you see what is going on at each step of the function in the console/dev tools
